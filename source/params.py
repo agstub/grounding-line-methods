529b98559b30343f6d286bc30bd011c4fa4acfa4
Aaron Stubblefield <ags2189@columbia.edu>
2020-07-14 17:14:27 -0400

    fixed small error in params file

--- source/params.py
+++ source/params.py
@@ -77,7 +77,7 @@ t_final =1*3.154e7                 # Final time (yr*sec_per_year).
 nt = int(nt_per_year*t_final/3.154e7) # Number of time steps
 dt = t_final/nt                       # Timestep size
 
-nx = 10*Nx
+nx = 4*int(Lngth/DX_s)
 X_fine = np.linspace(0,Lngth,num=nx)  # Horizontal coordinate for computing surface
                                       # slopes and plotting.
 
